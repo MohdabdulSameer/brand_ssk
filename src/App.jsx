302b382c61443887d18835c842c9603ba933890c
ssksameer313@gmail.com <111484563+MohdabdulSameer@users.noreply.github.com>
2024-07-11 13:11:42 +0530

    create it

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import Navigation from './component/Navigation';
+import HeroSection from './component/Hero';
+
+const App = () => {
+  return (
+    <div>
+    <Navigation/>
+    <HeroSection/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,27 @@
+const HeroSection =() =>{
+    return(
+    <main className="Hero container">
+
+        <div className="Hero-content">
+            <h1>SALEEM SAHAB FROM KODANGAL</h1>
+            <p>ALL Spare parts of vehicles like Tractors cars bus lorry and MRF & CEAT TIRES and etc</p>
+
+            <div className="Hero-btn">
+                <button>Shop Now</button>
+                <button className="Secondary-btn">Contact : 8106327442</button>
+            </div>
+
+            <div className="Shopping">
+                <p></p>
+
+            </div>
+        </div>
+
+        <div className="Hero-image">
+        <img src="./images/shop_image.png.jpg" alt='Shop-logo' style={{width:"400px"}}/>
+        </div>
+        
+    </main>
+    )
+}
+export default HeroSection;
